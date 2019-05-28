d467127aa1170c0b5e4e1a266fdc653771495058
MariusKurbakovas <marius.kurbakovas@gmail.com>
2019-05-28 16:24:05 +0300

    add create tripEmployee view (not finished)

---
+++ PSK/PSK.FrontEnd/Static/ViewTexts.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PSK.Domain.Enums;
+
+namespace PSK.FrontEnd.Static
+{
+    public static class ViewTexts
+    {
+        public static readonly List<SelectListItem> PurchasableStatusItems = new List<SelectListItem>
+        {
+            new SelectListItem(PurchasableStatus.NotNeeded.ToString(), PurchasableStatus.NotNeeded.ToString()),
+            new SelectListItem(PurchasableStatus.Requested.ToString(), PurchasableStatus.Requested.ToString()),
+            new SelectListItem(PurchasableStatus.Purchased.ToString(), PurchasableStatus.Purchased.ToString())
+        };
+    }
+}
