8539c6d9bee547ea5e765f4d87f13b8dfeaffdab
MariusKurbakovas <marius.kurbakovas@gmail.com>
2019-05-28 12:46:20 +0300

    can navigate to offices from trips

--- PSK/PSK.DataAccess/TripDataAccess.cs
+++ PSK/PSK.DataAccess/TripDataAccess.cs
@@ -19,12 +19,12 @@ namespace PSK.DataAccess
 
         public async Task<IEnumerable<Trip>> GetAll()
         {
-            return await _context.Trips.ToListAsync();
+            return await _context.Trips.Include(x => x.Employees).Include(x => x.EndLocation).Include(x => x.StartLocation).ToListAsync();
         }
 
         public async Task<Trip> Get(Guid id)
         {
-            return await _context.Trips.FirstOrDefaultAsync(x => x.Id == id);
+            return await _context.Trips.Include(x => x.Employees).Include(x => x.EndLocation).Include(x => x.StartLocation).FirstOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task<Trip> Add(Trip trip)
--- PSK/PSK.Domain/Office.cs
+++ PSK/PSK.Domain/Office.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PSK.Domain
 {
     public class Office : Entity
     {
+        [StringLength(60, MinimumLength = 3)]
         public string Name { get; set; }
 
         public Address Address { get; set; }
