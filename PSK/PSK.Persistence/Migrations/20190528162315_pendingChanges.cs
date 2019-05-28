d1124e5ce1be44f5b96d98d3ffcc355df0a4cb36
MariusKurbakovas <marius.kurbakovas@gmail.com>
2019-05-28 19:27:01 +0300

    migration fix

---
+++ PSK/PSK.Persistence/Migrations/20190528162315_pendingChanges.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PSK.Persistence.Migrations
+{
+    public partial class pendingChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accommodations_TripEmployees_TripEmployeeId",
+                table: "Accommodations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_TripEmployees_TripEmployeeId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_TripEmployeeId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accommodations_TripEmployeeId",
+                table: "Accommodations");
+
+            migrationBuilder.DropColumn(
+                name: "TripEmployeeId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TripEmployeeId",
+                table: "Accommodations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TripEmployeeId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TripEmployeeId",
+                table: "Accommodations",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_TripEmployeeId",
+                table: "AspNetUsers",
+                column: "TripEmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accommodations_TripEmployeeId",
+                table: "Accommodations",
+                column: "TripEmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accommodations_TripEmployees_TripEmployeeId",
+                table: "Accommodations",
+                column: "TripEmployeeId",
+                principalTable: "TripEmployees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_TripEmployees_TripEmployeeId",
+                table: "AspNetUsers",
+                column: "TripEmployeeId",
+                principalTable: "TripEmployees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
