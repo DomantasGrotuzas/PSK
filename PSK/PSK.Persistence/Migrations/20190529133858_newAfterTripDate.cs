2b7553d717748980f1ff9a57dde1fb57ac605e23
Augustas Juknevicius <juk.augustas@gmail.com>
2019-05-29 16:42:01 +0300

    update db

---
+++ PSK/PSK.Persistence/Migrations/20190529133858_newAfterTripDate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PSK.Persistence.Migrations
+{
+    public partial class newAfterTripDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
