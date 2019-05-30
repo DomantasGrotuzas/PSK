68af97049169435ac99aa4fe3402ba6dd6b88360
MariusKurbakovas <marius.kurbakovas@gmail.com>
2019-05-30 13:23:56 +0300

    add filtering to all trips

---
+++ PSK/Contracts/AllTripsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PSK.Domain;
+
+namespace Contracts
+{
+    public class AllTripsDto
+    {
+        public IEnumerable<Trip> AllTrips { get; set; }
+
+        public DateFilter DateFilter { get; set; }
+    }
+}
