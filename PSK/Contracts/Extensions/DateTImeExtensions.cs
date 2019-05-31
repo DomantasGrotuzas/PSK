41084097a2b0354bc4b48b7c028a887cdd929653
Domantas Grotuzas <Domantas.Grotuzas@visma.com>
2019-05-31 13:15:39 +0300

    FIx dates

---
+++ PSK/Contracts/Extensions/DateTImeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contracts.Extensions
+{
+    public static class DateTImeExtensions
+    {
+        public static DateTime ClearHours(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0);
+        }
+    }
+}
